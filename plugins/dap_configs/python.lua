a37912262186b152a6c17867e84234c6a327dc17
Yin Wang <wang.yin86520@gmail.com>
2022-11-10 09:27:38 -0500

    Change python command in debugpy.

--- plugins/dap_configs/python.lua
+++ plugins/dap_configs/python.lua
@@ -1,7 +1,7 @@
 local dap = require('dap')
 dap.adapters.python = {
  type = 'executable';
- command = "python3";
+ command = vim.fn.stdpath('data')..'/mason/packages/debugpy/venv/bin/python3';
  args = { '-m', 'debugpy.adapter' };
 }
 
